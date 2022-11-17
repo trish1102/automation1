80af7d6cc56d700c69b2728ec9fe5d9762def39d
trupti <truptisharma105@gmail.com>
2022-11-17 20:04:22 +1300

    login added successfully

---
+++ automation1/automation1/Program.cs
@@ -0,0 +1,34 @@
+﻿//open browser
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+//enter url
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+//enter username in username textbox
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+//enter password in password textbox
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+//click on login button 
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+//check its transfer you not dashboard or not
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if (hellohari.Text == "Hello hari!")
+{
+    Console.WriteLine("you passed the test");
+}
+else
+{
+    Console.WriteLine("you failed the test");
+}
+
+
+
+
+
+
+//
